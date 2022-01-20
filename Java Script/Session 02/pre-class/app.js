65f3b6c04007307d51420eaa93b557fbc3fb979e
savasgormus <savasgormus1@gmail.com>
2022-01-20 11:47:49 +0300

    first commit

---
+++ Java Script/Session 02/pre-class/app.js
@@ -0,0 +1,29 @@
+var myNumber = 3;
+document.write(myNumber)
+
+var x=5;
+var x=7;
+y=x;
+console.log(y);
+
+// --- --- ---
+
+var a = 10;
+{
+    let b = 3;
+}
+
+console.log("a = " + a);
+// console.log("b = " + b); => generates error 
+
+const xx = 5;
+// xx = 7; => generates error
+
+
+let firstName = "John", lastName = "clarusway", age = 40;
+console.log(firstName+lastName);
+
+var a;
+
+a = 2;
+console.log(a);
