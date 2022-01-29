447cd09050a4756d710271248b688af9618e2d39
savasgormus <savasgormus1@gmail.com>
2022-01-29 13:22:04 +0300

    update

--- Java Script/Session 04/in-class/02 loops.js
+++ Java Script/Session 04/in-class/02 loops.js
@@ -85,15 +85,47 @@ console.log({sum},{i});
 
 
 
+// while(true){
+//     let x = prompt(`bir sayı giriniz: \n q ile çıkış yapabilirsiniz`);
+//     if (x == "q") {
+//         console.log("çıkış yapıldı");
+//         break;
+//     } else if (isNaN(x)) {
+//         continue;
+//     } else{
+//         console.log(`${x}'in kares = ${x * x}`);
+//         //break;
+//     }
+// }
+
+
+// ----------- Math.random() metodu
+
+// console.log(Math.random()*6+1); // 1 ile 6 arasında sayı üretir.
+// console.log(Math.trunc(Math.random()*6)+1); // kesme işlemi yapar.
+// // özet: rastgele bir sayı ürettik ve küsüratını kesiyoruz.
+
+let x,y;
+let counter = 0;
+let çift = 0
+
 while(true){
-    let x = prompt(`bir sayı giriniz: \n q ile çıkış yapabilirsiniz`);
-    if (x == "q") {
-        console.log("çıkış yapıldı");
+    x = Math.trunc(Math.random()*6)+1;
+    y = Math.trunc(Math.random()*6)+1;
+    if (x == 6 && y == 6) {
+        counter++;
+        console.log(x,y, "kazandınız");
         break;
-    } else if (isNaN(x)) {
-        continue;
-    } else{
-        console.log(`${x}'in kares = ${x * x}`);
-        //break;
+    } else if (x == y && x != 6) {
+        çift++;
+        console.log(x,y,`${çift}. çift zar`);
+        if (çift == 3) {
+            console.log("3 çift zar kaybettiniz");
+            break;
+        }
     }
+    console.log(x,y);
+    counter++;
 }
+
+console.log(`${counter} defa tekrarlandı`);	
