a710db1fb7ad78362409fb52e42a09df54f62843
savasgormus <savasgormus1@gmail.com>
2022-02-03 18:11:22 +0300

    w3schools-arrays

--- w3schools/javascript/arrays/app.js
+++ w3schools/javascript/arrays/app.js
@@ -1,11 +1,128 @@
+    // --- 1 ---
+
 var cars = ["Saab", "Volvo", "BMW"];
-document.getElementById("demo1").innerHTML = cars;
+document.getElementById("demo1").innerHTML = cars;  // Saab, Volvo, BMW
+
+
+    // --- 2 ---
 
 var cars =[]
 cars[0] = "Saab";
 cars[1] = "Volvo";
 cars[2] = "BMW";
-document.getElementById("demo2").innerHTML = cars;
+document.getElementById("demo2").innerHTML = cars;  // Saab, Volvo, BMW
+
+
+    // --- 3 ---
 
 var cars = new Array("Saab", "Volvo", "BMW");
-document.getElementById("demo3").innerHTML = cars;
+document.getElementById("demo3").innerHTML = cars;  // Saab, Volvo, BMW
+
+
+    // --- 4 --- accessing array element
+
+var car = cars[0];
+document.getElementById("demo4").innerHTML = car;  // Saab
+
+
+    // --- 5 --- changing an array element
+
+var cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+document.getElementById("demo5").innerHTML = cars;  // Opel,Volvo,BMW
+
+
+    // --- 6 --- Arrays are Objects
+
+var person = ["John", "Doe", 46];
+document.getElementById("demo6").innerHTML = person[0];  // john
+console.log(typeof person); // object
+
+
+    // --- 7 --- Arrays are Objects
+
+var person = {firstName:"John", lastName:"Doe", age:46};
+document.getElementById("demo7").innerHTML = person.firstName;  // John
+console.log(typeof person);  // object
+
+
+    // --- 8 --- lenght property
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var lenght = fruits.length;
+document.getElementById("demo8").innerHTML = lenght;  // 4
+
+
+    // --- 9 --- Accessing the First Array Element
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruit = fruits[0];
+document.getElementById("demo9").innerHTML = fruit;  // Banana
+
+
+    // --- 10 --- Accessing the Last Array Element
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruit = fruits[fruits.length - 1];
+document.getElementById("demo10").innerHTML = fruit;  // Mango
+
+
+    // --- 11 --- Looping Array Elements
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fLen = fruits.length;
+
+var text = "<ul>";
+for (var i = 0; i < fLen; i++) {
+    text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+
+document.getElementById("demo11").innerHTML = text;  // <ul><li>Banana</li><li>Orange</li><li>Apple</li><li>Mango</li></ul>
+
+
+    // --- 12 --- Looping Array Elements
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var text = "<ul>";
+fruits.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+    text += "<li>" + value + "</li>"
+}
+
+document.getElementById("demo12").innerHTML = text;  // <ul><li>Banana</li><li>Orange</li><li>Apple</li><li>Mango</li></ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- w3schools/javascript/arrays/mynotes.md
+++ w3schools/javascript/arrays/mynotes.md
@@ -1,3 +1,41 @@
-4. boş bir array tanımladım. daha sonra index numaralarıyla elemanları ekledim.
+örnek 11:
 
-10. new Array yazarak array oluşturduk. [] yerine () kullanarak yapıyoruz.
+<!-- 
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fLen = fruits.length;
+
+var text = "<ul>";
+for (var i = 0; i < fLen; i++) {
+    text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+
+document.getElementById("demo11").innerHTML = text;   -->
+ <!-- <ul><li>Banana</li><li>Orange</li><li>Apple</li><li>Mango</li></ul> -->
+
+ önce fruits isimli bir array oluşturduk. daha sonra bu arrayin uzunluğunu fLen isimli bir değişkene
+ atadık.
+
+bir text değişkeni atadık ve bu değişkenin içine "ul" etiketini koyduk.
+
+for döngüsü içerisine alıyoruz. (i'yi 0'a atadık ve i'nin değeri fLen'in değerinden büyük olana kadar i'yi bir arttır --- var i = 0; i < fLen; i++)
+
+text değişkeninin içine "li" etiketi koy ve içindeki fruits arrayindeki i. elemanını yaz daha sonra "/li" ile etiketi kapat.
+
+for döngüsü bittikten sonra text'e "/ul" etiketi koy.
+
+örnek 12:
+<!-- 
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var text = "<ul>";
+fruits.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+    text += "<li>" + value + "</li>"
+}
+
+document.getElementById("demo12").innerHTML = text; -->
+
+yukarıdaki gibi önce arrayimizi ve text değişkenini oluşturduk. forEach metodu ile arrayimizi myFunction ismini verdiğimiz bir fonksiyona soktuk. bir alt satırda da text'e += ile "/ul" etiketi koyduk.
+forEach ile for döngüsünü uzun uzun yazmadan yukarıdaki işlemin aynısını yapabiliriz.
