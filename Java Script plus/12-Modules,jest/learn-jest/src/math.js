1e57477575e58df9a9c8878933788f0c61d9ff5b
savasgormus <savasgormus1@gmail.com>
2022-03-19 13:36:27 +0300

    update

--- Java Script plus/12-Modules,jest/learn-jest/src/math.js
+++ Java Script plus/12-Modules,jest/learn-jest/src/math.js
@@ -1,5 +1,17 @@
 const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8;
 
-const celsiusToFahrenheit = (temp) => temp *1.8 + 32;
+const celsiusToFahrenheit = (temp) => temp * 1.8 + 32;
 
 const calculateTotal = (price, vat = 0.18) => price * (1 + vat);
+
+const fibonacci = (num = 1) => {
+  //
+
+  const series = [1, 1];
+  for (let i = 2; i < num; i++) {
+    const a = series[i - 1];
+    const b = series[i - 2];
+    series.push(a + b);
+  }
+  return series[num - 1];
+};
