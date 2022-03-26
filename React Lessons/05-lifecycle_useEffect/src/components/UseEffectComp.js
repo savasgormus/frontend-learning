8d7f85ed3f243c90611815a5aaf1923c727eb0b3
savasgormus <savasgormus1@gmail.com>
2022-03-26 11:58:38 +0300

    lesson start

---
+++ React Lessons/05-lifecycle_useEffect/src/components/UseEffectComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseEffectComp = () => {
+  return (
+    <div className="function">
+      <h2>Functional Component</h2>
+    </div>
+  )
+}
+
+export default UseEffectComp
---
+++ React Lessons/05-lifecycle_useEffect/src/components/UseStateComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseStateComp = () => {
+  return (
+    <div className="function">
+      <h2>Functional Component</h2>
+    </div>
+  )
+}
+
+export default UseStateComp
