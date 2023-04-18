b9654d336e66583dc5f403911907e468abc1b102
savasgormus <savasgormus1@gmail.com>
2023-04-18 13:21:44 +0300

    update

---
+++ Java Script/02  - Data Types/datatypes.js
@@ -0,0 +1,5 @@
+console.log('js is running');
+
+//* var, let, const bir değişkeni deklare etme şekliydi.
+//* bir projeye başladığımızda default olarak const ile başlarız. değiştirmemiz gerekiyorsa let ile tanımlarız.
+//* 
