bf652e5cf2506c8ea8fc8d66586570b4fe58879c
savasgormus <savasgormus1@gmail.com>
2023-04-18 14:05:05 +0300

    update

--- Java Script/02  - Data Types/datatypes.js
+++ Java Script/02  - Data Types/datatypes.js
@@ -2,4 +2,44 @@ console.log('js is running');
 
 //* var, let, const bir değişkeni deklare etme şekliydi.
 //* bir projeye başladığımızda default olarak const ile başlarız. değiştirmemiz gerekiyorsa let ile tanımlarız.
-//* 
+
+//* JS dinamik bir dil olduğu için bir değişken atadığımızda data tipini otomatik olarak belirler. 
+//* örneğin bir tam sayı girdiğimizde bunu integer olarak belirtmemize gerek yok.
+
+// let mynumber; => define 
+// let myNumber = 10; => assign
+
+//! Operators
+//*typeof operatörü ile datanın tipini görebiliriz.
+
+// console.log(`1: ${typeof 0}`);
+
+// let e= prompt('bir şey yazınız');
+// console.log(typeof e);
+//* prompt ile girdiğimiz değerin türünü gördük.
+//* kullanıcıdan aldığımız veri her zaman stringtir.
+
+
+//? UNDEFINED
+//* bir değer ataması yapılmamış veri tipidir. 
+
+// let age;
+// console.log(age); //* output: "undefined"
+// console.log(userName); //* userName is not defined. hatadır.
+//* birisi undefined bir data type. diğeri ise hiç tanımlanmamış yani bir hata.
+
+// console.log(a); //* hata alacağız
+// var a = 3; //* a değeri daha sonradan tanımlandığı için undefined değeri verecek bize.
+
+// console.log(b);  
+// let b = 1; //* let ile tanımladığımız için bir önceki durumdaki gibi undefined değeri değil direkt hata alırız.
+
+//* var ile tanımladığımız değişkenler hoist olur ve en başta yüklenir, hazırda bekler. let ve const'da bu yoktur.
+
+//? NULL
+
+
+
+
+
+
