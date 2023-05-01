ed441e58032b2bb8bb441e599d6878cd1625cf3d
savasgormus <savasgormus1@gmail.com>
2023-05-01 20:36:11 +0300

    update

---
+++ Java Script/02  - Datatypes-Operators/02-operators.js
@@ -0,0 +1,85 @@
+        //! OPERATORS
+
+
+    //? Assigment Operators
+
+// let x = 10;
+// let y = 2;
+
+// x = y;
+// console.log(x); // 2
+
+// x += y; //* x = x + y;
+// console.log(x); // 12;
+
+// x -= y; //* x = x - y;
+// console.log(x); //* 8
+
+// x *= y; //* x = x * y;
+// console.log(x); //* 20
+
+// x /= y; //* x = x /y;
+// console.log(x); //* 5
+
+// x %= y; //* x = x % y;
+// console.log(x); //* 0
+
+// x **=y; //* x = x ** y;
+// console.log(x); //* 100
+
+    //? Aritmetic Operators
+
+// let x = 'hello ';
+// let y = 'world';
+// let z = 3;
+
+// console.log(x + y);
+// console.log(x + z);
+
+// //* postfix increment ve decrement
+
+// let a = 10;
+// let b = a++;
+
+// console.log(a); //* 11
+// console.log(b); //* 10
+
+//* a'nın değerini aldı b'ye atadı ve a'yı 1 yükseltti. bu durumda sadece a'nın yeni değeri değişti ve b sabit kaldı.
+
+// let c = 10;
+// let d = c--;
+// console.log(c); //* 9
+// console.log(d); //* 10
+
+//* prefix increment ve decrement ise bu işlemin tam tersidir. önce arttırma ya da azaltma işlemi yapar, daha sonra atamayı gerçekleştirir.
+
+// let a1 = 10;
+// let b1 = ++a1;
+// console.log(a1, b1); //* 11, 11
+
+
+    //? Comparison Operators
+
+//* karşılaştırma operatörleri bize her zaman true ya da false döndürür.
+
+//* equality 3 == '3'; true number ya da string olması farketmez
+//* inequality 3 != '3'; false
+//* strict equality 3 === '3'; false çünkü birisi number diğeri string
+//* strict inequality 3 !== '3'; true yine number ve string olduğu için eşit değiller. yani true.
+
+// let x = 5;
+
+// console.log(`(x == 8) -> ${x == 8}`);
+// console.log(`(x == '5') -> ${x == '5'}`);
+// console.log(`(x === '5') -> ${x === '5'}`);
+// console.log(`(x !== 8) -> ${x !== 8}`);
+// console.log(`(x < 8) -> ${x < 8}`);
+// console.log(`(x <= 4) -> ${x <=4}`);
+// console.log(`(x > '6') -> ${x > '6'}`);
+// console.log(`(x >= '9') -> ${x <= '9'}`);
+
+    //? Logical Operators
+
+//* And &&, verilen tüm değerler 1 ise true döndürür. aksi halde false döndürür. ilk değer 0 ise false döndürür.
+//* Or ||, verilen değerlerden sadece birisi 1 ise true döndürür.
+//* Not !, verilen değerin tam tersine göre true ya da false döndürür.
